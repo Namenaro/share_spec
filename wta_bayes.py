5b0656cfbe2e37ccbae97274f9dc0cad080d9406
neuro <nanenaro@gmail.com>
2017-05-22 13:20:31 +0300

    test advi

--- wta_bayes.py
+++ wta_bayes.py
@@ -10,6 +10,8 @@ import pymc3 as pm
 import pylab
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+os.chdir('results')
 
 def show_sample(rv, name_str):
     samples = rv.random(size=8000)
@@ -19,31 +21,39 @@ def show_sample(rv, name_str):
     plt.show()
 
 def show_data(data):
-    plt.hist(data, bins=70, normed=True, histtype='stepfilled')
+    plt.hist(data, bins=10, normed=True, histtype='stepfilled')
     plt.title("data")
     pylab.savefig("data" + '.png')
     plt.show()
 
-data = np.random.normal(size= 10, loc=2.)
-show_data(data)
+def show_posterior(v_params, data, some_str):
+    ax = sns.distplot(data, label='data')
+    xlim = ax.get_xlim()
+    x = np.linspace(xlim[0], xlim[1], 100)
+    y = stats.norm(v_params.means['mu'], v_params.stds['mu']).pdf(x)
+    ax.plot(x, y, label='ADVI')
+    ax.set_title('mu')
+    ax.legend(loc=0)
+    pylab.savefig('mu_'+some_str + '.png')
+   # plt.show()
+
+def get_data():
+    return np.random.normal(size=10, loc=2.)
 
 with pm.Model() as model:
-    my_mu = pm.Normal('mu', mu=0, sd=20)
+    my_mu = pm.Normal('mu', mu=0, sd=120)
     my_sd = pm.HalfNormal('sd', sd=10)
 
 show_sample(my_mu, "mu_prior")
 show_sample(my_sd, "sd_prior")
 
-with model:
-    n = pm.Normal('n', mu=my_mu, sd=my_sd, observed=data)
-    v_params = pm.variational.advi(model=model, n=10000)
-
-ax = sns.distplot(data, label='data')
-xlim = ax.get_xlim()
-x = np.linspace(xlim[0], xlim[1], 100)
-y = stats.norm(v_params.means['mu'], v_params.stds['mu']).pdf(x)
-ax.plot(x, y, label='ADVI')
-ax.set_title('mu')
-ax.legend(loc=0)
-pylab.savefig('foo.png')
-plt.show()
+test_val=0
+for n in range(10):
+    data = get_data()
+    iter_name = "iter_" + str(n)
+    with model:
+        my_mu.testval = test_val
+        n = pm.Normal(iter_name, mu=my_mu, sd=my_sd, observed=data)
+        v_params = pm.variational.advi(model=model, n=2000)
+        show_posterior(v_params, data, iter_name)
+        test_val = v_params.means['mu']
